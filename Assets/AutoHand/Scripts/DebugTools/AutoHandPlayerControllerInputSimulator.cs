535b46160d334854453c1f2b1f552913e35feb99
0x78f1935 <codewars.nl@gmail.com>
2022-03-19 05:18:08 +0100

    Refactor->Changed hand control movement; Updated documentation; Added first person view; Fixed grab functionality; Added reset keybinding; Added ability to crouch

--- Assets/AutoHand/Scripts/DebugTools/AutoHandPlayerControllerInputSimulator.cs
+++ Assets/AutoHand/Scripts/DebugTools/AutoHandPlayerControllerInputSimulator.cs
@@ -1,73 +1,100 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Autohand;
-using UnityEngine.Experimental.XR.Interaction;
 using UnityEngine.XR.Management;
 using UnityEngine.SpatialTracking;
 
 public class AutoHandPlayerControllerInputSimulator : MonoBehaviour
 {
-    public AutoHandPlayer player;
-    public float headHeight = 1.8f;
-    public KeyCode controlLeftHandKey = KeyCode.Q;
-    public KeyCode controlRightHandKey = KeyCode.E;
-    public bool isSimulating = false;
 
-    private bool simulate = false;
-    private SimulatedPoseDriver leftPoser, rightPoser, head;
-    private Vector2 screenSize;
-    private bool firstFrame = true;
+    [Header("Requirements")]
+    public AutoHandPlayer player;  // Autohand class available to the AutoHandPlayer prefab
+    public Hand devHandLeft; // The left hand of the debug tool
+    public Transform leftDevHandOffset; // The offset of the left dev hand
+    public Hand devHandRight; // The right hand of the debug tool
+    public Transform rightDevHandOffset; // The offset of the right dev hand
+
+    [Header("Key-Bindings")]
+    public float mouseSensitivity = 100.0f; // Mouse sensitivity 
+    public KeyCode controlLeftHandKey = KeyCode.Q;  // Key assignment for left hand
+    public KeyCode controlRightHandKey = KeyCode.E; // Key assignment for right hand
+    public KeyCode resetHandKey = KeyCode.R; // Key assignment for resetting hand pos
+    public KeyCode crouchKey = KeyCode.LeftControl; // Key assignment for crouching
+    public KeyCode primaryButtonKey = KeyCode.Mouse0; // Primary trigger button
+
+    [Header("Camera")]
+    public float fov = 80.0f;  // Field of View used in first person camera
+
+    [Header("Misc")]
+    public float headHeight = 1.8f;  // The simulated height of the Mock HMD
+
+    // -- Control variables
+    private bool simulate = false;  // True when script is allowed to run
+    private SimulatedPoseDriver leftPoser, rightPoser, head;  // placeholder (hands, head)
+
+    // -- First person view
+    private float rotY = 0.0f; // rotation around the up/y axis, used in first person camera
+    private float rotX = 0.0f; // rotation around the right/x axis, used in first person camera
 
     private void Start()
-    {
+    {  // Wait a fraction of a second to make sure the headset hardware is loaded in properly
         Invoke("DelayedStart", 0.1f);
     }
 
     private void DelayedStart()
     {
         if (!(bool)XRGeneralSettings.Instance?.Manager?.activeLoader.name.Contains("Mock"))
-        {
+        { // Makes sure all the requirements are met before starting this script
             return;
         }
         else
+        { // Set the script indicator on True
             simulate = true;
+        }
 
-        isSimulating = true;
+        if (simulate)
+        {  // Turn off the offset of the hands since this gives issues with raycasting
+            leftDevHandOffset.position = new Vector3(0, 0, 0);
+            rightDevHandOffset.position = new Vector3(0, 0, 0);
+            devHandLeft.transform.position = new Vector3(0, 0, 0);
+            devHandRight.transform.position = new Vector3(0, 0, 0);
+
+            Cursor.lockState = CursorLockMode.Confined;   // keep confined to center of screen
+        }
+
 
         var trackedPoseDrivers = FindObjectsOfType<TrackedPoseDriver>();
 
         foreach (var driver in trackedPoseDrivers)
         {
             if (driver.poseSource == TrackedPoseDriver.TrackedPose.Center)
-            {
+            { // Create a head
                 head = new GameObject("HeadDriver").AddComponent<SimulatedPoseDriver>();
                 head.transform.position = new Vector3(0, headHeight, 0);
                 driver.poseProviderComponent = head;
             }
             if (driver.poseSource == TrackedPoseDriver.TrackedPose.LeftPose)
-            {
+            { // Create a left hand pose
                 leftPoser = new GameObject("LeftDriver").AddComponent<SimulatedPoseDriver>();
                 leftPoser.transform.position = new Vector3(-.2f, 1f, 0.3f);
                 driver.poseProviderComponent = leftPoser;
             }
             if (driver.poseSource == TrackedPoseDriver.TrackedPose.RightPose)
-            {
+            { // Create a right hand pose
                 rightPoser = new GameObject("RightDriver").AddComponent<SimulatedPoseDriver>();
                 rightPoser.transform.position = new Vector3(.2f, 1f, 0.3f);
                 driver.poseProviderComponent = rightPoser;
             }
         }
-
+        // Obtain player
         player = GetComponent<AutoHandPlayer>();
+        // Activate head tracking
         Invoke("MoveHeadToStartTracking", 1f);
     }
 
 
 #if UNITY_EDITOR
-    Vector3 previousMousePos = Vector3.zero;
     enum Move
-    {
+    { // Contains possible outcomes of moving elements
         dontMove,
         bodyAndHead,
         leftHand,
@@ -79,9 +106,11 @@ public class AutoHandPlayerControllerInputSimulator : MonoBehaviour
 
     void Update()
     {
+        // If script is not allowed to run, deny service
         if (!simulate)
             return;
 
+        // Check what we are moving
         move = DetermineWhatToMove();
 
         switch (move)
@@ -89,8 +118,11 @@ public class AutoHandPlayerControllerInputSimulator : MonoBehaviour
             case Move.dontMove:
                 break;
             case Move.bodyAndHead:
+                player.allowBodyPushing = false;
+                player.allowClimbing = false;
+                player.allowClimbingMovement = false;
+                player.allowPlatforms = false;
                 HandleBodyMovement();
-                HandleMouseHeadRotation();
                 break;
             case Move.leftHand:
                 HandleHandControl(Move.leftHand);
@@ -105,9 +137,14 @@ public class AutoHandPlayerControllerInputSimulator : MonoBehaviour
             default:
                 break;
         }
+    }
 
-        previousMousePos = Input.mousePosition; // to have the delta mousePos
-
+    private void LateUpdate()
+    {
+        // If we do not move the hands we would like to freeze the camera so it (and the object we try to interact with) stays into frame
+        if (!(Input.GetKey(controlLeftHandKey) | Input.GetKey(controlRightHandKey)))
+        { HandleMouseHeadRotation(); }
+        UserInput();
     }
 
     void FixedUpdate()
@@ -119,8 +156,38 @@ public class AutoHandPlayerControllerInputSimulator : MonoBehaviour
             player.Move(GetMovementControls());
     }
 
+    Quaternion GetRotationTargetBasedOnMouse() 
+    { // Set target rotation to mouse pointer in 3D space
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = -Input.GetAxis("Mouse Y");
+
+        rotY += mouseX * mouseSensitivity * Time.deltaTime;
+        rotX += mouseY * mouseSensitivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -fov, fov);
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        return localRotation;
+    }
+
+    private Vector3 GetPostionTargetBasedOnMouse() 
+    {  // Converts mouse position to 3D position
+        var hand = move == Move.leftHand ? leftPoser : rightPoser;
+        var target_hand = devHandLeft;
+        if (hand.transform.name == "RightDriver")
+        {
+            target_hand = devHandRight;
+        }
+
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, 100))
+        {
+            return new Vector3(hit.point.x, hit.point.y, hit.point.z);
+        }
+        return target_hand.transform.position;
+    }
+
     Move DetermineWhatToMove()
-    {
+    { // Check what inputs are provided to determine what we are moving
         var controlLeftHand = Input.GetKey(controlLeftHandKey);
         var controlRightHand = Input.GetKey(controlRightHandKey);
 
@@ -137,70 +204,29 @@ public class AutoHandPlayerControllerInputSimulator : MonoBehaviour
     }
 
     void HandleBodyMovement()
-    {
+    {  // Handels player movement W, A, S, D
         player.Move(GetMovementControls());
     }
 
     void HandleMouseHeadRotation()
-    {
-        if (!Input.GetKey(KeyCode.Mouse1))
-        {
-            firstFrame = true;
-            return;
+    {  // Handels where the user is looking at in first person
+        if (head) { 
+            head.transform.rotation = GetRotationTargetBasedOnMouse();
         }
-
-        if (!firstFrame)
-        {
-            var deltaRot = GetMouseInput(previousMousePos, Input.mousePosition);
-            player.AddRotation(Quaternion.Euler(0, deltaRot.x * 500f, 0));
-            head.transform.Rotate(-deltaRot.y * 500f, 0, 0, Space.Self);
-        }
-
-        firstFrame = false;
-
     }
 
     void HandleHandControl(Move move)
-    {
-        var hand = move == Move.leftHand ? leftPoser : rightPoser;
-        var zDelta = Input.mouseScrollDelta.y / 50f;
-        var xyDelta = GetMouseInput(previousMousePos, Input.mousePosition)*3f;
-        hand.transform.position += new Vector3(xyDelta.x, xyDelta.y, zDelta);
-
-        if (!Input.GetKeyDown(KeyCode.Mouse0))
-            return;
-
-        if(move == Move.leftHand)
+    { // Moves hands to correct position
+        var hand = move == Move.leftHand ? leftPoser : rightPoser;  // Check which hand we currently using
+        Vector3 target = GetPostionTargetBasedOnMouse(); // Fetch target position
+        if (hand.transform.name == "LeftDriver") 
         {
-            if(player.handLeft.GetHeldGrabbable() != null)
-            {
-                player.handLeft.Release();
-            }   
-            else
-            {
-                player.handLeft.Grab();
-            }
-                
+            devHandLeft.SetHandLocation(target, GetRotationTargetBasedOnMouse());
         }
-        else if (move == Move.rightHand)
+        else if (hand.transform.name == "RightDriver")
         {
-            if (player.handRight.GetHeldGrabbable() != null)
-            {
-                player.handRight.Release();
-            }
-            else
-            {
-                player.handRight.Grab();
-            }
-                
-        }
-    }
-
-    private Vector2 GetMouseInput(Vector3 previousMousePos, Vector3 mousePos)
-    {
-        screenSize = new Vector2((float)Screen.currentResolution.height, (float)Screen.currentResolution.width);
-        var mouseDiff = (Vector2)(mousePos - previousMousePos);
-        return mouseDiff / screenSize;
+            devHandRight.SetHandLocation(target, GetRotationTargetBasedOnMouse());
+        }        
     }
 
     Vector2 GetMovementControls()
@@ -215,8 +241,77 @@ public class AutoHandPlayerControllerInputSimulator : MonoBehaviour
     public void MoveHeadToStartTracking()
     {
         head.transform.position += new Vector3(0, 0.01f, 0);
+        // Set firstperson view rotation
+        Vector3 rot = head.transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+    }
+
+    private void UserInput() 
+    { // handles user input
+        if (Input.GetKeyDown(crouchKey)) 
+        { // Allows the user to crouch
+            player.crouching = true;
+        }
+        else if (Input.GetKeyUp(crouchKey)) 
+        { // Stand up
+            player.crouching = false;
+        }
+
+        if (Input.GetKey(resetHandKey))
+        { // Reset hands in viewport
+            devHandLeft.ResetHandLocation();
+            devHandRight.ResetHandLocation();
+        }
+
+        if (move == Move.leftHand)
+        {
+            if (Input.GetKeyDown(primaryButtonKey))
+            {
+                if (devHandLeft.GetHeldGrabbable() != null)
+                {
+                    Debug.Log("Releasing");
+                    devHandLeft.Release();
+                }
+                else
+                {
+                    Debug.Log("Grabbing");
+                    devHandLeft.Grab();
+                }
+            }
+        
+        }
+        else if (move == Move.rightHand)
+        {
+            if (Input.GetKeyDown(primaryButtonKey))
+            { 
+                if (devHandRight.GetHeldGrabbable() != null)
+                {
+                    devHandRight.Release();
+                }
+                else
+                {
+                    devHandRight.Grab();
+                }
+            }
+        }
+        else if (move == Move.bothHands)
+        {
+            if (Input.GetKeyDown(primaryButtonKey))
+            {
+                if (devHandRight.GetHeldGrabbable() != null && devHandLeft.GetHeldGrabbable() != null)
+                {
+                    devHandRight.Release();
+                    devHandLeft.Release();
+                }
+                else
+                {
+                    devHandRight.Grab();
+                    devHandLeft.Grab();
+                }
+            }
+        }
     }
 
 #endif
 }
-
--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # AutoHandSimulator
 A keyboard/mouse input to simulate movement and hand controls for the [AutoHand](https://assetstore.unity.com/packages/tools/physics/auto-hand-vr-physics-interaction-165323) package for Unity.
 
-![ Alt text](AutoHandSim.gif) 
+![AutohandSim](AutoHandSim.gif) 
 
 (Do you like the looks of it? - it's a work in progress. Follow me on [twitter](https://twitter.com/MTrobotics?ref_src=twsrc%5Etfw) , I will be sharing more about this game in the coming months :-) )
 
 ## Update! - Now includes basic hand control
 
-![ Alt text](AutoHandSim_w_hands.gif) 
+![LegacyAutoHandSim](AutoHandSim_w_hands.gif) 
 
 To control hands, simply hold down Q for left hand and E for right hand and move the mouse, to move the hands up/down/left/right and scroll to move forward/backwards.
 
@@ -15,8 +15,11 @@ To control hands, simply hold down Q for left hand and E for right hand and move
 * Setup [AutoHand](https://assetstore.unity.com/packages/tools/physics/auto-hand-vr-physics-interaction-165323) for Unity.
 * Copy `Assets` folder into your project
 * Make sure `Mock HMD` is enabled: `Go to Edit -> Project Settings-> XR Plug-in Management -> Mock HMD Loader`
-* Attach the `AutoHandPlayerControllerInputSimulator.cs` to the **AutoHandPlayer**.
-* If using unity version **2021.2.12f1**, make sure to enable the old input handler: `Go to Edit -> Project Settings->Player->` Under `Other Settings` under Configuration is the option `Active Input Handling`. Select `Both`.
+* Attach the `Assets/AutoHand/Scripts/DebugTools/AutoHandPlayerControllerInputSimulator.cs` to the **AutoHandPlayer**.
+
+![DefaultConfig](ScriptLocation.png)
+
+* If using unity version **2021.2.12f1**, make sure to enable the old input handler: `Go to Edit` -> `Project Settings` -> `Player` -> Under `Other Settings` under `Configuration` is the option `Active Input Handling`. Select `Both`.
 
 The script should(!) detect whether the Mock HMD is used and only be effective, if that is the case. 
 If that is **NOT** the case, the code will simply not run. 
@@ -29,5 +32,64 @@ Requires:
 * The [AutoHand](https://assetstore.unity.com/packages/tools/physics/auto-hand-vr-physics-interaction-165323) package
 
 ## Known issues
-In the Demo scene locomotion using the script occasionally does not work. I have not been able to reliably repreoduce the problem - a restart of Unity usually fixes it.
+- In the Demo scene locomotion using the script occasionally does not work. I have not been able to reliably repreoduce the problem - a restart of Unity usually fixes it.
+- The raytracing of the hands when moving the hands is a little bit off, just because the screen is split in two in VR. You have to get used to it.
 
+## Controls
+Controls can be rebound to different key-bindings
+
+- W,A,S,D -> Body movement
+- HOLD Q-> Left hand movement
+- Q + Scroll up -> Move Left hand away
+- Q + Scroll down -> Move Left hand closer
+- HOLD E -> Right hand movement
+- E + Scroll up -> Move Right hand away
+- E + Scroll down -> Move Right hand closer
+- HOLD E + Q -> Both hands movement
+- E + Q + Scroll up -> Move both hands away
+- E + Q + Scroll down -> Move both hands closer
+- R -> Reset hands to default position
+- HOLD Q + LMB -> grab with left
+- HOLD E + LMB -> grab with right
+- HOLD Q + HOLD E + LMB -> grab with both hands
+
+- ESC -> Once play testing, click inside the game view to lock your cursor to the game.
+         Escape can be used to detach the cursor from the game window
+
+The camera behaves just like a first person shooter. When moving the mouse the camera freezes and stays in position.
+
+## Settings
+
+![DefaultConfig](DefaultConfiguration.png)
+
+| Category     | Setting                  | Description                                                               |
+|--------------|--------------------------|---------------------------------------------------------------------------|
+| Requirements | `Auto Hand Player`       | A ref to the script `Auto Hand Player`                                    |
+| Requirements | `Dev Hand Left`          | The left `Hand` script                                                    |
+| Requirements | `Left Dev Hand Offset`   | The `Transform` of the left hand offset                                   |
+| Requirements | `Dev Hand Right`         | The right `Hand` script                                                   |
+| Requirements | `Right Dev Hand Offset`  | The `Transform` of the right hand offset                                  |
+| Key-Bindings | `Mouse Sensitivity`      | The sensitivity of the first person camera                                |
+| Key-Bindings | `Control Left Hand` Key  | When hold, allows you to move the left hand to the position of the mouse  |
+| Key-Bindings | `Control Right Hand` Key | When hold, allows you to move the right hand to the position of the mouse |
+| Key-Bindings | `Reset Hand` Key         | When pressed reset the position of both hands relative to the headset     |
+| Key-Bindings | `Crouch` Key             | Player is crouching when pressed                                          |
+| Key-Bindings | `Primary Button` Key     | Button used to grab and release objects in the workspace                  |
+| Camera       | `FOV`                    | Field of View                                                             |
+| Misc         | `Head Height`            | The height of the player in Meters                                        |
+
+## Contributions
+
+New to old:
+
+---
+- Refactor -> [0x78f1935](https://github.com/0x78f1935)
+    - Changed the way hands move (by raycast)
+    - Added First Person view
+    - Fixed grab functionality
+    - Added reset keybinding
+    - Added crouch keybinding
+---
+- Base Code / idea -> [MThogersen](https://github.com/MThogersen)
+    - Initial commit
+---
